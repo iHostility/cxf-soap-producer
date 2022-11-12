5c5097328aeba0875c2b51fb0579ef79909af1b4
Denis Voloshin <dvoloshin@licard.com>
2022-11-12 15:14:39 +0300

    Initial commit

---
+++ src/main/java/su/hostile/cxfsoapproducer/CxfSoapProducerApplication.java
@@ -0,0 +1,13 @@
+package su.hostile.cxfsoapproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxfSoapProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxfSoapProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/su/hostile/cxfsoapproducer/CxfSoapProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package su.hostile.cxfsoapproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxfSoapProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
